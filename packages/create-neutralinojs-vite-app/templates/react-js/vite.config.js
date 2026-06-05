d6b42fc2b19b6b5ed073703f2891022f1821efb4
Ankit Mohanty <ankitmsb760@gmail.com>
2026-06-05 06:49:06 +0530

    feat(templates/react-js): implement auto port resolution dev script

--- packages/create-neutralinojs-vite-app/templates/react-js/vite.config.js
+++ packages/create-neutralinojs-vite-app/templates/react-js/vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true, // fail if port 5173 is taken — avoids silent port shift breaking Neutralinojs devUrl
-  },
+
   build: {
     outDir: "dist",
     emptyOutDir: true,
